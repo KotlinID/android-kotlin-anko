e068d2778143a201392c641941b15a13d0453c9b
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2018-02-05 11:05:41 +0700

    Add sample for intent callers share

--- app/src/main/java/id/kotlin/sample/anko/basic/callers/type/ShareActivity.kt
+++ app/src/main/java/id/kotlin/sample/anko/basic/callers/type/ShareActivity.kt
@@ -5,10 +5,13 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import id.kotlin.sample.anko.R
 import id.kotlin.sample.anko.dsl.ShareUI
 import org.jetbrains.anko.find
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.share
 
 class ShareActivity : AppCompatActivity() {
 
@@ -20,6 +23,17 @@ class ShareActivity : AppCompatActivity() {
         toolbar.title = title
         toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.bg_arrow_back)
         setSupportActionBar(toolbar)
+
+        val editTextSubject = find<EditText>(R.id.et_subject)
+        val editTextMessage = find<EditText>(R.id.et_message)
+        val button = find<Button>(R.id.btn_share)
+        button.setOnClickListener {
+            val subject = editTextSubject.text.toString()
+            val message = editTextMessage.text.toString()
+            when {
+                subject.isNotBlank() && message.isNotBlank() -> share(message, subject)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
--- app/src/main/java/id/kotlin/sample/anko/dsl/ShareUI.kt
+++ app/src/main/java/id/kotlin/sample/anko/dsl/ShareUI.kt
@@ -1,6 +1,8 @@
 package id.kotlin.sample.anko.dsl
 
 import android.support.v4.content.ContextCompat
+import android.text.InputType
+import android.view.inputmethod.EditorInfo
 import id.kotlin.sample.anko.R
 import id.kotlin.sample.anko.basic.callers.type.ShareActivity
 import org.jetbrains.anko.AnkoComponent
@@ -15,6 +17,8 @@ import org.jetbrains.anko.bottomPadding
 import org.jetbrains.anko.button
 import org.jetbrains.anko.design.coordinatorLayout
 import org.jetbrains.anko.dip
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.hintTextColor
 import org.jetbrains.anko.matchParent
 import org.jetbrains.anko.relativeLayout
 import org.jetbrains.anko.scrollView
@@ -44,7 +48,41 @@ class ShareUI : AnkoComponent<ShareActivity> {
                 scrollView {
                     isFillViewport = true
 
-                    verticalLayout {}
+                    verticalLayout {
+                        editText {
+                            id = R.id.et_subject
+                            hint = ctx.resources.getString(R.string.hint_subject)
+                            hintTextColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                            textColor = ContextCompat.getColor(ctx, R.color.colorPrimaryDark)
+                            textSize = 16f
+                            isCursorVisible = false
+                            imeOptions = EditorInfo.IME_ACTION_DONE
+                            inputType = InputType.TYPE_CLASS_TEXT
+                        }.lparams {
+                            width = matchParent
+                            height = wrapContent
+                            topMargin = dip(8)
+                            marginEnd = dip(16)
+                            marginStart = dip(16)
+                        }
+
+                        editText {
+                            id = R.id.et_message
+                            hint = ctx.resources.getString(R.string.hint_message)
+                            hintTextColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                            textColor = ContextCompat.getColor(ctx, R.color.colorPrimaryDark)
+                            textSize = 16f
+                            isCursorVisible = false
+                            imeOptions = EditorInfo.IME_ACTION_DONE
+                            inputType = InputType.TYPE_CLASS_TEXT
+                        }.lparams {
+                            width = matchParent
+                            height = wrapContent
+                            topMargin = dip(8)
+                            marginEnd = dip(16)
+                            marginStart = dip(16)
+                        }
+                    }
                 }.lparams {
                     below(R.id.toolbar_share)
                     above(R.id.btn_share)
