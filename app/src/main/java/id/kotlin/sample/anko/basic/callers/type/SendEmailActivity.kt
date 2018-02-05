b8b8849c358826d4b8c0bd72a13de9c0d25a35ba
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2018-02-05 16:27:22 +0700

    Add sample for intent callers send email

--- app/src/main/java/id/kotlin/sample/anko/basic/callers/type/SendEmailActivity.kt
+++ app/src/main/java/id/kotlin/sample/anko/basic/callers/type/SendEmailActivity.kt
@@ -5,8 +5,11 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
 import id.kotlin.sample.anko.R
 import id.kotlin.sample.anko.dsl.SendEmailUI
+import org.jetbrains.anko.email
 import org.jetbrains.anko.find
 import org.jetbrains.anko.setContentView
 
@@ -20,6 +23,19 @@ class SendEmailActivity : AppCompatActivity() {
         toolbar.title = title
         toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.bg_arrow_back)
         setSupportActionBar(toolbar)
+
+        val editTextEmail = find<EditText>(R.id.et_email)
+        val editTextSubject = find<EditText>(R.id.et_subject)
+        val editTextMessage = find<EditText>(R.id.et_message)
+        val button = find<Button>(R.id.btn_email)
+        button.setOnClickListener {
+            val emailaddress = editTextEmail.text.toString()
+            val subject = editTextSubject.text.toString()
+            val message = editTextMessage.text.toString()
+            when {
+                emailaddress.isNotBlank() && subject.isNotBlank() && message.isNotBlank() -> email(emailaddress, subject, message)
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
