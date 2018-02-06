fa867960c78facf193b59fb5ff61aec12b298a4c
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2018-02-06 21:45:19 +0700

    Add sample for Coroutines

--- app/src/main/java/id/kotlin/sample/anko/coroutine/CoroutinesActivity.kt
+++ app/src/main/java/id/kotlin/sample/anko/coroutine/CoroutinesActivity.kt
@@ -1,6 +1,7 @@
 package id.kotlin.sample.anko.coroutine
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.design.widget.CoordinatorLayout
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
@@ -11,8 +12,14 @@ import android.view.MenuItem
 import id.kotlin.sample.anko.R
 import id.kotlin.sample.anko.coroutine.CoroutinesAdapter.CoroutinesListener
 import id.kotlin.sample.anko.dsl.CoroutinesUI
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.asReference
+import org.jetbrains.anko.coroutines.experimental.bg
+import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.find
 import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.toast
 
 class CoroutinesActivity : AppCompatActivity(), CoroutinesListener {
 
@@ -52,8 +59,29 @@ class CoroutinesActivity : AppCompatActivity(), CoroutinesListener {
     override fun onCoroutineClick(position: Int) {
         val rootView = find<CoordinatorLayout>(R.id.container_coroutines)
         when (position) {
-            CoroutinesConfigs.COROUTINE_REFERENCE -> {}
-            CoroutinesConfigs.COROUTINE_BACKGROUND -> {}
+            CoroutinesConfigs.COROUTINE_REFERENCE -> {
+                toast("Processing data...")
+
+                val reference = this.asReference()
+                async(UI) {
+                    val result = "Coroutines are cool!"
+                    reference().showResult(result)
+                }
+            }
+            CoroutinesConfigs.COROUTINE_BACKGROUND -> {
+                snackbar(rootView, "Loading...")
+
+                async(UI) {
+                    val result = bg {
+                        "Welcome to Coroutines!"
+                    }.await()
+                    Handler().postDelayed({ snackbar(rootView, result) }, 3000L)
+                }
+            }
         }
+    }
+
+    private fun showResult(result: String) {
+        Handler().postDelayed({ toast(result) }, 3000L)
     }
 }
